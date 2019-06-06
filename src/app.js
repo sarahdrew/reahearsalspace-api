36f29a88c6d32c5957d11213c582bc531210f85b
Sarah Drew <sarahdrew93@gmail.com>
2019-06-05 19:28:17 -0500

    added cors

--- src/app.js
+++ src/app.js
@@ -13,6 +13,15 @@ const app = express();
 
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
+const cors = require('cors');
+const { CLIENT_ORIGIN } = require('./config');
+
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN
+  })
+);
+
 app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
