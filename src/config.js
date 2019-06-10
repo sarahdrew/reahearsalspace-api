4749aefaf15f51449af59a8ff01235a85d44a4a5
Sarah Drew <sarahdrew93@gmail.com>
2019-06-10 15:04:14 -0500

    updates to client origin

--- src/config.js
+++ src/config.js
@@ -3,4 +3,5 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   DB_URL: process.env.DATABASE_URL || "postgresql://postgres@localhost/rehearsalspace",
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:3000/api"
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000'
 };
